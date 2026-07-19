57d05f96cd3bae0c46e34f6b446d9db2569e3443
Mayank Singh <singhmani40922@gmail.com>
2026-07-19 22:42:35 +0530

    add buttons dynamically

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const paginationListWraper = document.querySelector(".pagination-list");
-
+const paginationNumbers = document.querySelector(".pagination-numbers");
 function createDummyData(){
     for(let i = 1; i<=100 ; i++){
         const li = document.createElement("li");
@@ -9,3 +9,23 @@ function createDummyData(){
 }
 createDummyData();
 
+const extractAllListItems = document.querySelectorAll("li");
+
+let paginationLimit = 10;
+let pageCount = Math.ceil(extractAllListItems.length/paginationLimit);
+
+function createPageNumber(getCurrentIndex){
+const pageNumber = document.createElement("button");
+pageNumber.classList.add("pagination-number");
+pageNumber.textContent = getCurrentIndex;
+pageNumber.setAttribute("page-index",getCurrentIndex);
+paginationNumbers.appendChild(pageNumber);
+}
+
+function createPaginationNumbers(){
+    for(let i = 1 ; i<=pageCount ; i++){
+        createPageNumber(i);
+    }
+}
+
+createPaginationNumbers();
