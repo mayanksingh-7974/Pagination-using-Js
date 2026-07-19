406784d7237c7f1dafac767c048255b4d7ac315a
Mayank Singh <singhmani40922@gmail.com>
2026-07-20 00:49:47 +0530

    add functionality in buttons

--- script.js
+++ script.js
@@ -9,10 +9,15 @@ function createDummyData(){
 }
 createDummyData();
 
-const extractAllListItems = document.querySelectorAll("li");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+const extractAllListItems =
+paginationListWraper.querySelectorAll("li");
 
 let paginationLimit = 10;
 let pageCount = Math.ceil(extractAllListItems.length/paginationLimit);
+let currentPage = 1;
 
 function createPageNumber(getCurrentIndex){
 const pageNumber = document.createElement("button");
@@ -28,4 +33,73 @@ function createPaginationNumbers(){
     }
 }
 
+function handelActiveCurrentPageNumber(){
+document.querySelectorAll(".pagination-number").forEach(button=>{
+    button.classList.remove("active-state");
+    const getCurrentPageIndex = Number(button.getAttribute("page-index"));
+    if(getCurrentPageIndex === currentPage){
+ button.classList.add("active-state");
+    }
+})
+}
+
+function handelDisableButton(getBtn){
+getBtn.classList.add("disabled");
+getBtn.setAttribute("disabled","true");
+}
+
+function handelEnableButton(getBtn){
+getBtn.classList.remove("disabled");
+getBtn.removeAttribute("disabled");
+}
+
+function handelPaginationButtonStatus(){
+if(currentPage === 1){
+    handelDisableButton(prevBtn);
+}
+else{
+    handelEnableButton(prevBtn);
+}
+if(pageCount === currentPage){
+    handelDisableButton(nextBtn);
+}
+else{
+    handelEnableButton(nextBtn);
+}
+}
+
+function handelCurrentPage(getCurrentPageNumber){
+    currentPage = getCurrentPageNumber;
+
+    handelActiveCurrentPageNumber();
+    handelPaginationButtonStatus();
+
+    let getPreviousRange = (getCurrentPageNumber-1)*paginationLimit;
+    let getCurrentRange = getCurrentPageNumber*paginationLimit;
+
+    extractAllListItems.forEach((listItem,index)=>{
+listItem.classList.add("hide-list-item");
+
+if(index>=getPreviousRange && index<getCurrentRange){
+    listItem.classList.remove("hide-list-item");
+}
+    });
+}
 createPaginationNumbers();
+handelCurrentPage(1);
+
+prevBtn.addEventListener("click",()=>{
+    handelCurrentPage(currentPage-1);
+});
+nextBtn.addEventListener("click",()=>{
+    handelCurrentPage(currentPage+1);
+});
+
+document.querySelectorAll(".pagination-number").forEach(button=>{
+    const getCurrentPageIndex = Number(button.getAttribute("page-index"));
+    if(getCurrentPageIndex){
+        button.addEventListener("click",()=>{
+            handelCurrentPage(getCurrentPageIndex);
+        })
+    }
+})
